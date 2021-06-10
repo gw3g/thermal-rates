6ba1d52497074ef5b9bb06e15540b438224c9d4c
Greg J <jackson@itp.unibe.ch>
2021-06-09 19:48:48 -0700

    9.6.21

--- main.c
+++ main.c
@@ -19,14 +19,9 @@ double mubar = 6.283185307179586476925286766559; // = 2.pi
 
 char E = 'U';
 
-void check_ML(double,double,double,double,double);
-
 //FILE *in;
-FILE *out;
-void rho_f_scan(double);   // k input
+void check_ML(double,double,double,double,double);
 void virtual_check(double,double,double);   // omega,k,ma input
-void Rate_fig2_scan(double,double,double,double); // M, ml, mQ, mS
-void M_scan(double,double,double,double); // k, ml, mQ, mS
 
 #include <gsl/gsl_sf_bessel.h>
 double k2av(double M) {
@@ -40,15 +35,12 @@ double k2av(double M) {
 #include "Real.h"
 #include "Virt.h"
 #include "htl.h"
-#include "rho.h" // master SPF's
 
+#include "scan.h"
+#include "rho.h" // master SPF's
 
 /*--------------------------------------------------------------------*/
 
-
-
-
-void test_C0();
 int main () {
   //rho_j_virt(3.,1.,.1,.01,.4);
   //test_C0();
@@ -59,7 +51,7 @@ int main () {
   //M_scan(.5,.1,.01,.4);
   //check_ML(3.,0.96875,.1,.01,1.);
   //check_ML(.3,0.4,.1,.01,1.);
-  rho_j_scan1();
+  //rho_j_scan1();
 
   //double m_l = .1, m_Q = .01, m_S = 1.;
   E='U';
@@ -71,58 +63,21 @@ int main () {
   //Rate_fig2_scan(3.,.1,.01,10.);
   //Rate_fig2_scan(.3,.1,.01,1.);//*/
 
-  /*double aa[3] = {.1,0.,-1.};
-  double bb[3] = {.01,0.,+1.};
-  double cc[3] = {10.,0.,+1.};
-  double dd[3] = {0.,0.,-1.};//*/
-
-  //double ph = Phi(2.,1.1,aa,bb,0);
-  //printf("1 -> 2 = %g\n\n", ph);
-
-  //double ans1 = Rate_1_to_3(2.1,1.,aa,bb,cc);
-
-  //double ans2s = Rate_2_to_2_sChan(3.,.01,aa,bb,cc,_id);
-  //double ans2t = Rate_2_to_2_tChan(3.,.01,aa,bb,cc,_id);
-
-  //double ans3s = Rate_3_to_1_sChan(2.1,1.1,aa,bb,cc,_id);
-  //double ans3t = Rate_3_to_1_tChan(2.1,1.1,aa,bb,cc,_id);
-
   //printf("1 -> 3 = %g\n\n", ans1);
   //printf("2 -> 2 (s) = %g\n", ans2s);
   //printf("2 -> 2 (t) = %g\n\n", ans2t);
   //printf("3 -> 1 (s) = %g\n", ans3s);
   //printf("3 -> 1 (t) = %g\n", ans3t);//*/
-  //
-  /*
-  double o = .12, ans;
-  while (o<80.) {
-    ans = rho_f(o,.1,aa,bb,cc);
-    printf("omega = %g , rho_f = %g\n", o, ans);
-    o*=1.3;
-  }//*/
 
   //virtual_check(2.,1.1,.1);
   //virtual_check(2.,1.1,.01);
   //virtual_check(2.,1.1,.001);
   //virtual_check(2.,1.1,.0);
 
-  //double ans1 = Rate_1_to_2(2.,1.1,aa,bb,cc,dd);
-    //printf("omega = %g , rho_virt = %g\n", 2.1, ans1);
-
   //rho_f_scan(.0001);
   //rho_f_scan(.1);
   //rho_f_scan(1);
   //rho_f_scan(10);
-
-  //
-  /*double z = 2.;
-  double ab[2] = {1.,-.2};
-  double complex FF;
-  while (ab[1]<1.9) {
-    FF = calF(z,ab);
-    printf("z=%.2f, a=%.2f, b=%.2f, result = %.2f + I %.2f \n", z, ab[0], ab[1], creal(FF), cimag(FF));
-    ab[1]+=.1;
-  }*/
 
   return 0;
 }
@@ -179,114 +134,6 @@ void virtual_check(double o, double k, double ma) {
 
 }
 
-void Rate_fig2_scan(double M, double m_l, double m_Q, double m_S) {
-  double g1 = 1./3., g2 = 2./3., mu_L = 1e-3, mu_Y = 2.*1e-2;
-  double G12 = 2.*(SQR(g1)+3.*SQR(g2));
-
-  //double m_l = .1, m_Q = .01, m_S = 1.;
-  double mu_l = mu_L - .5*mu_Y, mu_Q = 0, mu_S = .5*mu_Y;
-  double l_[3] = {m_l, mu_l, -1.};
-  double Q_[3] = {m_Q, mu_Q, +1.};
-  double S_[3] = {m_S, mu_S, +1.};
-
-  double dm_S = 1e-7;
-  double St_plus[3]  = {m_S+dm_S, mu_S, +1.};
-  double St_minus[3] = {m_S-dm_S, mu_S, +1.};
-
-  int N_k;
-  double o, k, k_min, k_max, step;
-  double res_1_to_2, res_1_to_3, res_2_to_2, res_3_to_1, tot;
-  double res_virt;
-
-  char *prefix=(char*)"out/rates_";
-  char  suffix[50];
-  char  filename[90];
-
-  // filename
-  strcpy(filename,prefix);
-  sprintf(suffix,"E=%c_{ml=%.2f,mQ=%.3f,mS=%.2f,M=%.2f}.dat",E,m_l,m_Q,m_S,M);
-  strcat(filename,suffix);
-  out=fopen(filename,"w");
-
-  // Here are some parameters that can be changed:
-  N_k=150; 
-  k_min=0.001;
-  k_max=15.;
-  // don't change anything after that.
-
-  k = k_min;
-  step=(k_max-k_min)/((double) N_k-1);
-
-
-  printf(" Settings: M=%g, with k_min=%g, k_max=%g\n",M,k_min,k_max); 
-  double frac;
-  double ccc[3];
-
-  for (int i=0; i<N_k; i++) {
-    frac = (double)i/(double)(N_k-1);
-    o = sqrt( SQR(M) + SQR(k) );
-
-    res_1_to_2  = 0.;
-    res_1_to_2 += Phi(o,k,l_,S_,_448_);
-
-    res_1_to_3  = 0.;
-    res_1_to_3 += Rate_1_to_3(o,k,l_,Q_,S_,_B1_i );
-    res_1_to_3 += Rate_1_to_3(o,k,S_,Q_,l_,_B1_ii);
-
-    res_2_to_2  = 0.;
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,Q_,l_,S_,_45_i  );
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,l_,Q_,S_,_45_ii );
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,Q_,S_,l_,_45_iii);
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,S_,Q_,l_,_45_iii);
-
-    res_2_to_2 += Rate_2_to_2_sChan(o,k,S_,Q_,l_,_B9_i);
-    res_2_to_2 += Rate_2_to_2_sChan(o,k,l_,Q_,S_,_B9_ii);//*/
-
-    res_3_to_1  = 0.;
-    res_3_to_1 += Rate_3_to_1_sChan(o,k,Q_,l_,S_,_B27_i );
-    res_3_to_1 += Rate_3_to_1_sChan(o,k,Q_,S_,l_,_B27_ii);
-
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,l_,S_,Q_,_B19_i );
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,Q_,S_,l_,_B19_ii);
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,S_,l_,Q_,_B19_iii);
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,Q_,l_,S_,_B19_iii);//*/
-
-    res_virt  = 0.;
-    res_virt += Bubble_T(o,k,l_,Q_,S_,l_,_virt_i);
-    res_virt += Bubble_T(o,k,Q_,S_,l_,S_,_virt_ii);
-
-    res_virt += (m_S)*( Bubble_T(o,k,Q_,S_,l_,St_plus ,_virt_iii)
-                      - Bubble_T(o,k,Q_,S_,l_,St_minus,_virt_iii) )/(2.*dm_S);
-
-    res_virt += (SQR(m_S)-SQR(M))*Triangle_T(o,k,l_,Q_,S_,l_,S_,_virt_iv);
-
-    ccc[0] = 1.; ccc[1] = 0.; ccc[2] = 0.;
-    res_virt += Bubble_0(o,k,l_,Q_,S_,l_,ccc);
-    ccc[0] = 2.; ccc[1] = 1.; ccc[2] = 0.;
-    res_virt += (SQR(m_S)-SQR(M))*Triangle_0(o,k,l_,Q_,S_,l_,S_,ccc);
-    ccc[0] = -1.; ccc[1] = -1.; ccc[2] = 2.;
-    res_virt += Bubble_0(o,k,Q_,S_,l_,S_,ccc);
-    ccc[0] = -1.; ccc[1] = -1.; ccc[2] = 1.;
-    res_virt += (m_S)*( Bubble_0(o,k,Q_,S_,l_,St_plus,ccc)
-                      - Bubble_0(o,k,Q_,S_,l_,St_minus,ccc) )/(2.*dm_S);
-
-    tot = res_1_to_3+res_2_to_2+res_3_to_1+res_virt;
-    res_1_to_3 *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    res_2_to_2 *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    res_3_to_1 *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    res_virt   *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    tot *= G12;
-    //printf("omega = %g , rho_f = %g\n", o, ans);
-
-    printf(" k = %.5e , [%2.2f%]\n", k , 100.*frac); 
-    fprintf( out, "%.10e    %.10e    %.10e    %.10e    %.10e    %.10e    %.10e\n", 
-                   k, res_1_to_2, res_1_to_3, res_2_to_2, res_3_to_1, res_virt, tot );
-    k += step;
-  }
-
-  printf(" Saved to file ["); printf(filename); printf("]\n"); fclose(out);
-
-}
 
 void check_ML(double M, double k, double m_l, double m_Q, double m_S) {
   double g1 = 1./3., g2 = 2./3., mu_L = 1e-3, mu_B = 0., mu_Y = 2.*1e-2;
@@ -417,172 +264,3 @@ void check_ML(double M, double k, double m_l, double m_Q, double m_S) {
 
 }
 
-void test_C0() {
-  int nn = 100;
-  E = 'U';
-  double M, ma, mb, mc, md, me; // ml, mQ, mS, ml, mS
-  double M_i[6] = {.1e+3, .1e-2, .1e-2, .1e-1, .1e-2, .1e-1};
-  double M_f[6] = {3.,    .1,    .01, .1e+1, .1,    .1e+1};
-  double M_step[6];
-
-  for (int i=0;i<6;i++) { M_step[i] = (M_f[i]-M_i[i])/((double)nn-1); }
-  M  = M_i[0];
-  ma = M_i[1];
-  mb = M_i[2];
-  mc = M_i[3];
-  md = M_i[4];
-  me = M_i[5];
-  double ccc[3] = {1.,0.,1.};
-  double g1 = 1./3., g2 = 2./3., mu_L = 1e-3, mu_B = 0., mu_Y = 2.*1e-2;
-  double G12 = 2.*(SQR(g1)+3.*SQR(g2));
-
-  //double m_l = .1, m_Q = .01, m_S = 1.;
-  double mu_l = mu_L - .5*mu_Y, mu_Q = 0., mu_S = .5*mu_Y;
-  double l_[3] = {0., mu_l, -1.};
-  double Q_[3] = {0., mu_Q, +1.};
-  double S_[3] = {0., mu_S, +1.};
-  double o, k =1.;
-  double C0_new, C0_old;
-
-
-  for (int i=0;i<nn;i++) {
-    //l_[0] = ma;
-    //Q_[0] = mb;
-    //S_[0] = mc;
-    //o = sqrt(M*M+k*k);
-    //C0 = G12*(SQR(mc)-SQR(M))*Triangle_0(o,k,l_,Q_,S_,l_,S_,ccc)/o;
-    //C0 = Triangle_0(o,k,l_,Q_,S_,l_,S_,ccc)/o;
-    C0_old = L3(SQR(M),SQR(ma),SQR(mb),SQR(mc),SQR(md),SQR(me))*SQR(OOFP);
-    C0_new = L3_new(SQR(M),SQR(ma),SQR(mb),SQR(mc),SQR(md),SQR(me))*SQR(OOFP);
-
-    printf("%.8e   %.8e   %.8e   %.8e   %.8e   %.8e   %.8e   %.8e\n",
-            md,me,M,ma,mb,mc,C0_new,C0_old);
-    M  += M_step[0];
-    ma += M_step[1];
-    mb += M_step[2];
-    mc += M_step[3];
-    md += M_step[4];
-    me += M_step[5];
-
-  }
-}
-
-void M_scan(double k, double m_l, double m_Q, double m_S) {
-  double g1 = 1./3., g2 = 2./3., mu_L = 1e-3, mu_Y = 2.*1e-2;
-  double G12 = 2.*(SQR(g1)+3.*SQR(g2));
-
-  //double m_l = .1, m_Q = .01, m_S = 1.;
-  //double mu_l = mu_L - .5*mu_Y, mu_Q = 0, mu_S = .5*mu_Y;
-  double mu_l = 0., mu_Q = 0., mu_S = 0.;
-  double l_[3] = {m_l, mu_l, -1.};
-  double Q_[3] = {m_Q, mu_Q, +1.};
-  double S_[3] = {m_S, mu_S, +1.};
-  double mlT = (1.+SQR(mu_l/M_PI))*G12/(32.); mlT = sqrt(mlT);
-
-  double dm_S = 1e-5;
-  double St_plus[3]  = {m_S+dm_S, mu_S, +1.};
-  double St_minus[3] = {m_S-dm_S, mu_S, +1.};
-
-  int N_M;
-  double o, M, M_min, M_max, step;
-  double res_1_to_2, res_1_to_3, res_2_to_2, res_3_to_1, tot;
-  double res_virt;
-
-  char *prefix=(char*)"out/M_scan_";
-  char  suffix[50];
-  char  filename[90];
-
-  //printf( "2to2, HTL = %.8f\n", Rate_2_to_2_HTL(2.01,2.0,l_,S_,mlT) );
-
-  // filename
-  strcpy(filename,prefix);
-  sprintf(suffix,"E=%c_{ml=%.2f,mQ=%.3f,mS=%.2f,k=%.2f}.dat",E,m_l,m_Q,m_S,k);
-  strcat(filename,suffix);
-  out=fopen(filename,"w");
-
-  // Here are some parameters that can be changed:
-  N_M=300; 
-  M_min=0.01;
-  M_max=10.;
-  // don't change anything after that.
-
-  M = M_min;
-  step=pow(M_max/M_min,1./(N_M-1));
-
-
-  printf(" Settings: k=%g, with M_min=%g, M_max=%g\n",k,M_min,M_max); 
-  double frac;
-  double ccc[3];
-
-  for (int i=0; i<N_M; i++) {
-    frac = (double)i/(double)(N_M-1);
-    o = sqrt( SQR(M) + SQR(k) );
-
-    //res_1_to_2  = 0.;
-    //res_1_to_2 += Phi(o,k,l_,S_,_448_);
-
-    res_1_to_3  = 0.;
-    res_1_to_3 += Rate_1_to_3(o,k,l_,Q_,S_,_B1_i );
-    res_1_to_3 += Rate_1_to_3(o,k,S_,Q_,l_,_B1_ii);//*/
-
-    res_2_to_2  = 0.;
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,Q_,l_,S_,_45_i  );
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,l_,Q_,S_,_45_ii );
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,Q_,S_,l_,_45_iii);
-    res_2_to_2 += Rate_2_to_2_tChan(o,k,S_,Q_,l_,_45_iii);
-
-    res_2_to_2 += Rate_2_to_2_sChan(o,k,S_,Q_,l_,_B9_i);
-    res_2_to_2 += Rate_2_to_2_sChan(o,k,l_,Q_,S_,_B9_ii);//*/
-
-    res_3_to_1  = 0.;
-    res_3_to_1 += Rate_3_to_1_sChan(o,k,Q_,l_,S_,_B27_i );
-    res_3_to_1 += Rate_3_to_1_sChan(o,k,Q_,S_,l_,_B27_ii);
-
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,l_,S_,Q_,_B19_i );
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,Q_,S_,l_,_B19_ii);
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,S_,l_,Q_,_B19_iii);
-    res_3_to_1 += Rate_3_to_1_tChan(o,k,Q_,l_,S_,_B19_iii);//*/
-
-    res_virt  = 0.;
-    res_virt += Bubble_T(o,k,l_,Q_,S_,l_,_virt_i);
-    res_virt += Bubble_T(o,k,Q_,S_,l_,S_,_virt_ii);
-
-    res_virt += (m_S)*( Bubble_T(o,k,Q_,S_,l_,St_plus ,_virt_iii)
-                      - Bubble_T(o,k,Q_,S_,l_,St_minus,_virt_iii) )/(2.*dm_S);
-
-    res_virt += (SQR(m_S)-SQR(M))*Triangle_T(o,k,l_,Q_,S_,l_,S_,_virt_iv);
-
-    ccc[0] = 1.; ccc[1] = 0.; ccc[2] = 0.;
-    res_virt += Bubble_0(o,k,l_,Q_,S_,l_,ccc);
-    ccc[0] = 2.; ccc[1] = 1.; ccc[2] = 0.;
-    res_virt += (SQR(m_S)-SQR(M))*Triangle_0(o,k,l_,Q_,S_,l_,S_,ccc);
-    ccc[0] = -1.; ccc[1] = -1.; ccc[2] = 2.;
-    res_virt += Bubble_0(o,k,Q_,S_,l_,S_,ccc);
-    ccc[0] = -1.; ccc[1] = -1.; ccc[2] = 1.;
-    res_virt += (m_S)*( Bubble_0(o,k,Q_,S_,l_,St_plus,ccc)
-                      - Bubble_0(o,k,Q_,S_,l_,St_minus,ccc) )/(2.*dm_S);//*/
-
-    //res_2_to_2 += Rate_2_to_2_HTL(o,k,l_,S_,mlT);
-    res_1_to_2  = Rate_1_to_2_HTL(o,k,l_,S_,mlT);
-    res_1_to_2 += Rate_2_to_2_HTL(o,k,l_,S_,mlT);
-
-    //tot = res_1_to_3+res_2_to_2+res_3_to_1+res_virt + Rate_1_to_2_HTL(o,k,l_,S_,mlT);
-    res_1_to_3 *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    res_2_to_2 *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    res_3_to_1 *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    res_virt   *= G12;//2.*(SQR(g1)+3.*SQR(g2));
-    tot = res_1_to_3 + res_2_to_2 + res_3_to_1 + res_1_to_2 + res_virt;
-    //tot *= G12;
-    //printf("omega = %g , rho_f = %g\n", o, ans);
-
-    printf(" o = %.5e , [%2.2f%]\n", o , 100.*frac); 
-    //fprintf( out, "%.10e    %.10e    %.10e    %.10e    %.10e    %.10e    %.10e\n", 
-    //               M, res_1_to_2, res_1_to_3, res_2_to_2, res_3_to_1, res_virt, tot );
-    fprintf( out, "%.10e    %.10e    %.10e    %.10e    %.10e    %.10e    %.10e\n", 
-                   M, res_virt, res_1_to_2, res_1_to_3, res_2_to_2, res_3_to_1, tot );
-    M *= step;
-  }
-
-  printf(" Saved to file ["); printf(filename); printf("]\n"); fclose(out);
-
-}
